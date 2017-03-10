777de24ae1e93d4453adb54b131804716271464a
jpcanoso <jpcanoso@hotmail.com>
2017-03-10 19:36:41 +0000

    Update mysql.go

---
+++ mysql.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@/api")
+	if err != nil {
+		fmt.Print(err.Error())
+	} else {
+    fmt.Print("Ligado à BD \n")
+  }
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	type Person struct {
+		Id         int
+		First_Name string
+		Last_Name  string
+	}
+	router := gin.Default()
+
+	router.GET("/pessoas", func(c *gin.Context) {
+		var (
+			person  Person
+			persons []Person
+		)
+		rows, err := db.Query("select id, nome, apelido from dados1;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		for rows.Next() {
+			err = rows.Scan(&person.Id, &person.First_Name, &person.Last_Name)
+			persons = append(persons, person)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+		}
+		defer rows.Close()
+		c.JSON(http.StatusOK, gin.H{
+			"result": persons,
+			"count":  len(persons),
+		})
+	})
+
+	router.Run(":3000")
+}
